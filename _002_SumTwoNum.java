66a8b451cd2a0b30eae765900e851eaca0e91238
Basu Slathia <basuslathia01@gmail.com>
2025-01-15 22:38:52 +0530

    structure file

---
+++ _002_SumTwoNum.java
@@ -0,0 +1,10 @@
+public class _002_SumTwoNum {
+    public static void main(String[] args) {
+        
+        int a = 16;
+        int b = 17;
+        int c = a + b;
+
+        System.out.println("Sum is " + c);
+    }
+}
