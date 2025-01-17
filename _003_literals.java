2ea68214233944966e46541d9cf17135c5b4aeb0
Basu Slathia <basuslathia01@gmail.com>
2025-01-17 11:51:30 +0530

    literals

---
+++ _003_literals.java
@@ -0,0 +1,16 @@
+public class _003_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2= 56;
+        char ch ='A';
+        short age3= 87;
+        long ageDino= 566666666666l;
+        float f1 = 5.6f;
+        double d1= 4.66D;
+        boolean a = true;
+        System.out.println(age3);
+        String str = "Harry";
+        System.out.println(str);
+        
+    }
+}
