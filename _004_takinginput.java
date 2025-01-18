d53356e44b9a12b3371b953703121463951e6e07
Basu Slathia <basuslathia01@gmail.com>
2025-01-18 16:48:24 +0530

    file updated

--- _004_takinginput.java
+++ _004_takinginput.java
@@ -1,24 +1,24 @@
 
-// import java.util.Scanner;
-// public class _004_takinginput {
-//     public static void main(String[] args) {
-//         System.out.println("Taking input from the user ");
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter number 1");
-//         // int a = sc.nextInt();
-//         float a = sc.nextFloat();
-//         System.out.println("Enter number 2");
-//         // int b= sc.nextInt();
-//         float b = sc.nextFloat();
-//         // int sum= a+b;
-//         float sum = a+b;
+import java.util.Scanner;
+public class _004_takinginput {
+    public static void main(String[] args) {
+        System.out.println("Taking input from the user ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        // int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2");
+        // int b= sc.nextInt();
+        float b = sc.nextFloat();
+        // int sum= a+b;
+        float sum = a+b;
 
-//         System.out.println("The sum of these numbers is");
-//         System.out.println(sum);
+        System.out.println("The sum of these numbers is");
+        System.out.println(sum);
 
-//     }
+    }
     
-// }
+}
 
 // import java.util.Scanner;
 
