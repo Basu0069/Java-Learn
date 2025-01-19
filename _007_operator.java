ee7ba5670b500893419bf4abf7961ae6ec4123c5
Basu Slathia <basuslathia01@gmail.com>
2025-01-20 01:37:18 +0530

    file modified

---
+++ _007_operator.java
@@ -0,0 +1,24 @@
+public class _007_operator {
+    public static void main(String[] args) {
+        // Precedence & Associativity 
+        int a = 6*5 -34/2;
+        
+        /* 
+        =30-34/2
+        =30-17
+        =13
+         */
+
+         int b = 60/5-34*2;
+         /*
+            =12-34*2
+            =12-68
+            =-56
+          */
+
+        System.out.println(a);
+        System.out.println(b);
+        // Precedence & Associativity
+
+    }
+}
