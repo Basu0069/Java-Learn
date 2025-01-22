c418a9f4d504610f742eb503b788c769bd5c48c0
Basu Slathia <basuslathia01@gmail.com>
2025-01-22 10:57:16 +0530

    typecasting

---
+++ _009_typecasting.java
@@ -0,0 +1,13 @@
+public class _009_typecasting {
+ public static void main(String[] args) {
+     
+    char grade = 'B';
+    System.out.println(grade);
+
+
+    // Decrypting the grade
+    grade = (char)(grade -8);
+    System.out.println(grade);
+    
+ }   
+}
