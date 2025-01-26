d6b5db513cc7b97c5e53eef6719c99059f9d3e4f
Basu Slathia <basuslathia01@gmail.com>
2025-01-26 23:09:56 +0530

    string

---
+++ _010_string.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class _010_string {
+    public static void main(String[] args) {
+        
+        // String name = "Harry";
+        // System.out.print("The name is : ");
+        // System.out.print(name);
+        int a = 6;
+        float b = 5.6754f;
+        System.out.printf("The value of a is %d and value of b is %8.2f", a ,b);
+        // System.out.format("The value of a is %d and value of b is %f", a ,b);
+        Scanner sc= new Scanner(System.in);
+
+        String st= sc.nextLine();
+        System.out.println(st);
+
+
+    }
+}
---
+++ _011_String_method.java
@@ -0,0 +1,26 @@
+public class _011_String_method {
+    
+    public static void main(String[] args) {
+        String name = "Harry";
+        // // System.out.println(name);
+        int value = name.length();
+        System.out.println(value);
+
+        // String lstring= name.toLowerCase();
+        // System.out.println(lstring);
+
+        // String ustring= name.toUpperCase();
+        // System.out.println(ustring);
+
+
+        // // trim se extra spaces remove ho jati h 
+        // String nonTrimmedString = "        Harry            ";
+        // System.out.println(nonTrimmedString);
+        // String trimmedString = nonTrimmedString.trim();
+        // System.out.println(trimmedString);
+
+        System.out.println(name.substring(2));
+13.57
+         
+    }
+}
