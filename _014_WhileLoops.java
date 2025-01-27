d67f7757720b65fdd2571717ab631ca9e3af8141
Basu Slathia <basuslathia01@gmail.com>
2025-01-27 22:19:18 +0530

    while loop

---
+++ _014_WhileLoops.java
@@ -0,0 +1,14 @@
+public class _014_WhileLoops {
+    public static void main(String[] args) {
+        
+        System.out.println("Using Loops");
+        int i=100;
+
+        while(i<=200){
+            System.out.println(i);
+            i++;
+
+        }
+        System.out.println("Finish Running while loop");
+    }
+}
