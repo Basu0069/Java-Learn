68fecf336cae870de657ff717023106f58d264a7
Basu Slathia <basuslathia01@gmail.com>
2025-02-03 23:53:48 +0530

    break

---
+++ _017_Break.java
@@ -0,0 +1,59 @@
+public class _017_Break {
+    public static void main(String[] args) {
+        // break and continue using loops
+
+        // for(int i=0;i<5;i++){
+        //     System.out.println(i);
+        //     System.out.println("Java is great");
+
+        //     if(i==2){
+        //         System.out.println("Ending the loop");
+        //         break;
+        //     }
+        // }
+        // System.out.println("loops end here");
+
+
+
+
+
+
+
+
+
+
+        // int i=0;
+        // do{
+        //     System.out.println(i);
+        //     System.out.println("Java is great");
+        //     if(i==2){
+        //         System.out.println("Ending the loop");
+        //         break;
+        //     }
+        //     i++;
+        // }while(i<5);
+
+        // System.out.println("Loop ends here");
+
+
+
+
+
+
+
+
+        for(int i=0;i<50;i++){
+            if(i==2){
+                System.out.println("Ending the loop");
+                continue;
+            }
+            System.out.println(i);
+            System.out.println("Java is great");
+        }
+
+
+        
+
+
+    }
+}
