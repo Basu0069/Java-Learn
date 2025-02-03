d1618ce6c0f6f9356cb2f5732ac3ffd3fe163c5b
Basu Slathia <basuslathia01@gmail.com>
2025-02-03 23:55:15 +0530

    PracSet

---
+++ _018_PracSet.java
@@ -0,0 +1,70 @@
+// Q1: print star patter 
+
+
+
+public class _018_PracSet {
+ public static void main(String[] args) {
+     
+//     // Practice Problem 1 
+//     int n=4;
+//     for(int i=n;i>0;i--){
+//         for(int j=0;j<i;j++){
+//         System.out.print("*");
+//     }
+//     System.out.print("\n");
+//     }
+
+
+    // Practice Problem 2 --- First four even number sum 
+    // int sum=0;
+    // int n=4;
+    // for(int i=0;i<n;i++){
+    //     sum=sum+(2*i);
+    // }
+    // System.out.println("Sum of even numbers is: ");
+    // System.out.println(sum);
+    // 0 2 4 
+
+
+    // Practice Problem 3--- Table of 5 
+    // int n=5;
+    // for(int i=1;i<=10;i++){
+    //     System.out.printf("%d X %d = %d\n", n , i, n*i);
+
+    // }
+
+
+
+    // Practice Problem 4-> write factotrial using while loop 
+    // int n= 5;
+    // // 5!= 5*4*3*2*1
+
+    // int i=1;
+    // int factorial=1;
+
+    // while(i<=n){
+    //     factorial *= i;
+    //     i++;
+    //     }
+    // System.out.println(factorial);
+
+
+    // Practice Problem 5-> 
+    int n=8;
+    int sum=0;
+    // for(int i=0;i<10;i++)
+    for(int i=1;i<=10;i++){
+        sum+=n*i;
+    }
+    System.out.println(sum);
+
+
+
+ }   
+}
+
+
+
+
+
+
