db3d31376ad22d961cbd8dbb700dede0f29b50cb
Basu Slathia <basuslathia01@gmail.com>
2025-02-03 23:52:26 +0530

    forloop

---
+++ _016_ForLoop.java
@@ -0,0 +1,17 @@
+public class _016_ForLoop {
+    public static void main(String[] args) {
+        // for(int i=0;i<=10;i++){
+        //     System.out.println(i);
+        // }
+//  
+
+
+for(int i=5;i>0;i--){
+    System.out.println(i);
+}
+
+
+
+
+    }
+}
