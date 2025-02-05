a7bfa12575101245514ac75249e5a5787ca7b7e4
Basu Slathia <basuslathia01@gmail.com>
2025-02-05 21:45:54 +0530

    array

---
+++ _019_array.java
@@ -0,0 +1,30 @@
+public class _019_array {
+    public static void main(String[] args) {
+        
+        // classroom of 500 students- you have to store marks of these 500 students 
+
+        
+        // There are three main ways to create an array in java 
+        // 1.
+        // int marks[] =new int[5];
+        
+
+        // 2.
+        // int marks[];
+        // marks= new int[5];
+
+
+        // 3.
+        int marks[]= {98,45,34,66,79};
+        
+
+
+        marks[0]= 100;
+        marks[1]= 60;
+        marks[2]= 70;
+        marks[3]= 80;
+        marks[4]= 90;
+
+        System.out.println(marks[4]);
+    }
+}
