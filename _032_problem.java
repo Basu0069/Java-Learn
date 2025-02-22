b2f3a801f038a454d284bb034b6e970f874c1ffe
Basu Slathia <basuslathia01@gmail.com>
2025-02-22 23:53:33 +0530

    problems

---
+++ _032_problem.java
@@ -0,0 +1,17 @@
+
+
+public class _032_problem {
+public static void main(String[] args) {
+    
+
+
+    /*
+     * Create a class Game, which allows auser to play "Guess the number" game once .Game should have the follwiijg methods:
+     * 1.Constructor to generate the random number 
+     * 2. takeUserInput() to take a user input of number
+     * 3. isCorrectNumber() to detect whether the number enterred by the user is true 
+     * 4.getter and setter for noOfGuesses
+     * Use peoperties such as nofoGuesses(int), etc to get this task done!
+     */
+}    
+}
